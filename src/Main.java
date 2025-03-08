21a30cd051072159ea8842b8c7cb72098c1760cd
Pavel <Pahenmurman@gmail.com>
2025-03-08 14:21:06 +0300

    home work1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
+// 1-е задание
+        byte clientOs = 1;
+        switch (clientOs) {
+            case 0: System.out.println("Установите версию приложения для iOS по ссылке");
+            break;
+            case 1: System.out.println("Установите версию приложения для Android по ссылке");
+            break;
+            default: System.out.println("Приложение не может работать в Вашей ОС");
+        }
     }
 }
